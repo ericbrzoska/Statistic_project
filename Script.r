31ac03c50622fda62f63fdf720b6f7e437c3415a
Eric Brzoska <43241904+ericbrzoska@users.noreply.github.com>
2023-03-22 11:09:26 +0100

    Update Script.r
    Initiation is completed, now the coding can start

--- Script.r
+++ Script.r
@@ -1,2 +1,28 @@
-data = read.csv2("Downloads/arab.csv", sep=",")
+"Imports
+-> if you run the code the first time, uncommend the installation lines
+"
+#install.packages("here")
 
+library(here)
+
+"
+Initiation
+-> Shall make the import easier, so no one needs to change this path
+-> pls save the Github_folder in your Download directory
+"
+
+init= getwd()
+setwd(here("Downloads","Statistic_project-main"))     
+
+"
+Coding space
+-> it would be great to
+"
+
+data = t(read.csv2("arab.csv", sep=","))              # Reads .csv in and transposes it by t()
+
+
+"
+-> Switching work directory back to normal
+"
+setwd(init)
